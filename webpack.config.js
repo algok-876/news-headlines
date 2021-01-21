2a983414308b28f1a4f9571a5133bee8c1ab1745
AzuraXW <2219444825@qq.com>
2021-01-21 17:06:10 +0800

    项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,129 @@
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { resolve } = require('path')
+
+module.exports = {
+  // 开发模式
+  mode: 'development',
+  // source-map
+  devtool: 'source-map',
+  // 优化，禁止压缩
+  optimization: {
+    minimize: false
+  },
+  // 入口，多入口文件
+  entry: {
+    index: resolve(__dirname, './src/js/index.js'),
+    detail: resolve(__dirname, './src/js/detail.js'),
+    collections: resolve(__dirname, './src/js/collections.js')
+  },
+  // 输出打包设置
+  output: {
+    // 路径
+    path: resolve(__dirname, './dist'),
+    // 文件名
+    filename: 'js/[name]_[hash:8].js'
+  },
+  // 模块设置d
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: resolve(__dirname, 'node_modules'),
+        query: {
+          'presets': ['latest']
+        }
+      },
+      {
+        test: /\.tpl$/,
+        loader: 'ejs-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [autoprefixer('last 5 versions')]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [autoprefixer('last 5 versions')]
+              }
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|ico|woff|eot|svg|ttf)$/,
+        loaders: 'url-loader?limit=1024&name=img/[name]_[hash:16].[ext]'
+      }
+    ]
+  },
+  // 插件
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: resolve(__dirname, './src/index.html'),
+      title: '新闻头条-首页',
+      chunks: ['index'],
+      chunksSortMode: 'manual',
+      excludeChunks: ['node_modules'],
+      hash: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'detail.html',
+      template: resolve(__dirname, './src/index.html'),
+      title: '新闻头条-首页',
+      chunks: ['detail'],
+      chunksSortMode: 'manual',
+      excludeChunks: ['node_modules'],
+      hash: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'collections.html',
+      template: resolve(__dirname, './src/index.html'),
+      title: '新闻头条-首页',
+      chunks: ['collections'],
+      chunksSortMode: 'manual',
+      excludeChunks: ['node_modules'],
+      hash: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    })
+  ],
+  devServer: {
+    watchOptions: {
+      ignored: /node_modules/
+    },
+    open: true,
+    host: 'localhost',
+    port: 3000
+  }
+}
